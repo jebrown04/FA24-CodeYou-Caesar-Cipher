a69a5297e110dc34aa26b92bf54117cb02b71298
James E Brown <brownje0610@gmail.com>
2024-11-17 23:47:06 -0500

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,1 +1,19 @@
 # add your code here
+def caesar_cipher(text, shift=5):
+    encrypted_text = ""
+    
+    for char in text:
+        if char.isalpha():
+            start = 65 if char.isupper() else 97
+            encrypted_char = chr(start + (ord(char) - start + shift) % 26)
+            encrypted_text += encrypted_char
+        else:
+            
+            encrypted_text += char
+            
+    return encrypted_text
+
+input_message=input("Enter a message:")
+encrypted_text = caesar_cipher(input_message, 5)
+print(input_message)
+print(encrypted_text)
